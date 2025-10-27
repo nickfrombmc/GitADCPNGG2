7caa0d7715eb9428a629ce418702a118117f3bea
nguillem <nguillem@host.docker.internal>
2025-10-27 16:41:59 -0400

    stuff

--- COB/CWXTSUBC.cbl
+++ COB/CWXTSUBC.cbl
@@ -6,7 +6,7 @@
       *                                                                *
       *  THIS CALLED PROGRAM IS THE COBOL DEMO PROGRAM USED FOR        *
       *  XPEDITER/TSO TRAINING SESSIONS.MKS                            *
-      *  Updated comment - 10/27/25 - 003                              *
+      *  Updated comment - 10/27/25 - 004                              *
       *                                                                *
       ******************************************************************
        ENVIRONMENT DIVISION.
